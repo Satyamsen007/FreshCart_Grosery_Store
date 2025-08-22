05f22d8dfc5fe3d912e569e081656bda41460b15
Satyam <satyamsen280@gmail.com>
2025-08-22 12:50:33 +0530

    feat: update package dependencies and remove productionBrowserSourceMaps from configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'res.cloudinary.com', 'avatars.githubusercontent.com'],
   },
-  productionBrowserSourceMaps: false,
   // // Production optimizations
   // swcMinify: true,
   // compress: true,
